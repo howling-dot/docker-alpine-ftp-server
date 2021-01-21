8065790feaf455b4bfbb3bd1f4bfdde1a02eb9ef
Howling <73427651+howling-dot@users.noreply.github.com>
2021-01-21 15:11:57 +0200

    Update start_vsftpd.sh

--- start_vsftpd.sh
+++ start_vsftpd.sh
@@ -54,6 +54,22 @@ fi
 # Used to run custom commands inside container
 if [ ! -z "$1" ]; then
   exec "$@"
+  
+# If TLS flag is set and no certificate exists, generate it
+if [ ! -e /etc/vsftpd/private/vsftpd.pem ]; then
+  echo "Generating self-signed certificate"
+  mkdir -p /etc/vsftpd/private
+
+  openssl req -x509 -nodes -days 7300 \
+      -newkey rsa:2048 -keyout /etc/vsftpd/private/vsftpd.pem -out /etc/vsftpd/private/vsftpd.pem \
+      -subj "/C=FR/O=My company/CN=example.org"
+  openssl pkcs12 -export -out /etc/vsftpd/private/vsftpd.pkcs12 -in /etc/vsftpd/private/vsftpd.pem -passout pass:
+
+  chmod 755 /etc/vsftpd/private/vsftpd.pem
+  chmod 755 /etc/vsftpd/private/vsftpd.pkcs12
+fi
+
+
 else
   exec /usr/sbin/vsftpd -opasv_min_port=$MIN_PORT -opasv_max_port=$MAX_PORT $ADDR_OPT /etc/vsftpd/vsftpd.conf
 fi
